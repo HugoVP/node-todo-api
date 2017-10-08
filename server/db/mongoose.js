88aaec9f72af2dadbab35819dfdcf5df8f2917e0
HugoVP <dev@hugo.plus>
2017-10-08 15:21:47 -0500

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/Todo');
-const {User} = require('./models/User');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 const app = express();
 
@@ -25,24 +25,4 @@ app.listen(3000, () => {
   console.log(`Started on port: 3000`);
 });
 
-// /* Create a todo */
-// var todo = Todo({
-//   text: `Walk the dog`,
-// });
-//
-// todo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }).catch((err) => {
-//   console.log(`Unable to save: ${err}`);
-// });
-//
-// /* Create a user */
-// var user = new User({
-//   email: 'one@example.com',
-// });
-//
-// user.save().then((doc) => {
-//   console.log(`User saved: ${JSON.stringify(doc, undefined, 2)}`);
-// }).catch((err) => {
-//   console.log(`Unable to save: ${err}`);
-// });
+module.exports = {app};
