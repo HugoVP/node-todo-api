17a0c78ee460c3e971d60b679c45d93a0b8ffc05
HugoVP <dev@hugo.plus>
2017-10-09 17:33:13 -0500

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,11 +47,35 @@ app.get('/todos/:id', (req, res) => {
       if (!todo) {
         res.status(404).send();
       }
-
       else {
         res.send({todo});
       }
     })
+    .catch((err) => res.status(400).send(err));
+});
+
+app.delete('/todos/:id', (req, res) => {
+  /* Get the id */
+  const {id} = req.params;
+
+  /* Validate the id -> not valid? return 404 */
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+    return;
+  }
+
+  /* Remove todo by id */
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      /* If no doc, send 404 */
+      if (!todo) {
+        res.status(404).send();
+      }
+      /* Id doc, send doc back with 200 */
+      else {
+        res.send(todo);
+      }
+    })    
     .catch((err) => res.status(400).send(err));
 });
 
