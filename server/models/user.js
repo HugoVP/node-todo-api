7383eaea57d77a76670cafc85bb83dafe3f14246
HugoVP <dev@hugo.plus>
2017-10-10 16:20:29 -0500

    Add POST /users route & token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,62 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-/* User model */
-const User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
     minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email',
+    },
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+  }],
 });
+
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, [
+    '_id',
+    'email',
+  ]);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  const access = 'auth';
+
+  const token = jwt.sign({
+    _id: user._id.toHexString(),
+    access,
+  }, '123abc').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => token);
+};
+
+/* User model */
+const User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -85,6 +85,21 @@ app.patch('/todos/:id', (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+/* POST /users - Create a new user */
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, [
+    'email',
+    'password',
+  ]);
+
+  const user = new User(body);
+
+  user.save()
+    .then(() => user.generateAuthToken())
+    .then((token) => res.header('x-auth', token).send(user))
+    .catch((err) => res.status(400).send(err));
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Started on port: ${process.env.PORT}`);
 });
